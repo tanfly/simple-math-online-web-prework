46740091aa190fd28ff72e0f0a82730cda853608
Stella McEwen <mcewen.stella@gmail.com>
2019-04-05 19:11:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,14 +14,22 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+  puts num1*num2
+  num1*num2
 end
 
 def modulo(num1, num2)
+  puts num1%num2
+  num1%num2
 end
 
 def square_root(num)
+  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  puts num1 + ((num2*num3)/num4)
+  num1 + ((num2*num3)/num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
